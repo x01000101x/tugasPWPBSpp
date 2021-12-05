a42e5190d6219346c31134df4dcac2517df924ad
x01000101x <leodamanik619@gmail.com>
2021-12-05 17:59:55 +0700

    part 1

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -6,29 +6,23 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="<?php echo
-											base_url('assets/Login_v12/images/icons/favicon.ico'); ?>">
+	<link rel="icon" type="image/png" href="<?= base_url('assets/images/icons/favicon.ico'); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo
-													base_url('assets/Login_v12/vendor/bootstrap/css/bootstrap.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css'); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo
-													base_url('assets/Login_v12/fonts/font-awesome-4.7.0/css/font-awesome.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css'); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/Login_V12/fonts/Linearicons-Free-v1.0.0/icon-font.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css'); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/Login_V12/vendor/animate/animate.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/vendor/animate/animate.css'); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/Login_V12/vendor/css-hamburgers/hamburgers.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/vendor/css-hamburgers/hamburgers.min.css'); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/Login_V12/vendor/animsition/css/animsition.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/vendor/select2/select2.min.css'); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/Login_V12/vendor/select2/select2.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/util.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/main.css'); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/Login_V12/vendor/daterangepicker/daterangepicker.css'); ?>">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/Login_V12/css/util.css'); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/Login_V12/css/main.css'); ?>">
 </head>
 
 <body>
@@ -36,7 +30,7 @@
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('images/img-01.jpg');">
 			<div class="wrap-login100 p-t-190 p-b-30">
-				<form class="login100-form validate-form" action="<?= site_url('Login/auth'); ?>">
+				<form class="login100-form validate-form">
 					<div class="login100-form-avatar">
 						<img src="images/avatar-01.jpg" alt="AVATAR">
 					</div>
@@ -54,7 +48,7 @@
 					</div>
 
 					<div class="wrap-input100 validate-input m-b-10" data-validate="Password is required">
-						<input class="input100" type="password" name="password" placeholder="Password">
+						<input class="input100" type="password" name="pass" placeholder="Password">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock"></i>
@@ -88,21 +82,14 @@
 
 
 	<!--===============================================================================================-->
-	<script src="<?php echo base_url('assets/Login_V12/vendor/jquery/jquery-3.2.1.min.js'); ?>"></script>
+	<script src="<? base_url('vendor/jquery/jquery-3.2.1.min.js'); ?>"></script>
 	<!--===============================================================================================-->
-	<script src="<?php echo base_url('assets/Login_V12/vendor/animsition/js/animsition.min.js'); ?>"></script>
+	<script src=" <? base_url('vendor/bootstrap/js/popper.js'); ?>"></script>
+	<script src="<? base_url('vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
 	<!--===============================================================================================-->
-	<script src="<?php echo base_url('assets/Login_V12/vendor/bootstrap/js/popper.js'); ?>"></script>
-	<script src="<?php echo base_url('assets/Login_V12/vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
+	<script src=" <? base_url('vendor/select2/select2.min.js'); ?>"></script>
 	<!--===============================================================================================-->
-	<script src="<?php echo base_url('assets/Login_V12/vendor/select2/select2.min.js'); ?>"></script>
-	<!--===============================================================================================-->
-	<script src="<?php echo base_url('assets/Login_V12/vendor/daterangepicker/moment.min.js'); ?>"></script>
-	<script src="<?php echo base_url('assets/Login_V12/vendor/daterangepicker/daterangepicker.js'); ?>"></script>
-	<!--===============================================================================================-->
-	<script src="<?php echo base_url('assets/Login_V12/vendor/countdowntime/countdowntime.js'); ?>"></script>
-	<!--===============================================================================================-->
-	<script src="<?php echo base_url('assets/Login_V12/js/main.js'); ?>"></script>
+	<script src="<? base_url('js/main.js'); ?>"></script>
 
 </body>
 
